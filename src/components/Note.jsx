1237ee9506e8e50d19540cd0c771c7ad8cd4acd7
hamedpro <hamedpro30@gmail.com>
2022-11-18 11:27:39 +0330

    initializing note and task components

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-
+import { get_notes, get_note_sections } from '../../api/client'
+import ObjectBox from './ObjectBox'
 export const Note = () => {
-    var {note_id,workspace_id,workflow_id,username } = useParams()
+    var { note_id, workspace_id, workflow_id, username } = useParams()
+    var [note, set_note] = useState(null)
+    var [note_sections, set_note_sections] = useState(null)
+    async function get_data() {
+        var all_notes = await get_notes({ username })
+        set_note(all_notes.find(note => note._id == note_id))
+        set_note_sections(await get_note_sections({creator : username,note_id}))
+    }
+    useEffect(() => {
+        get_data()
+    },[])
   return (
-    <div>Note</div>
+      <div>
+          <h1>Note</h1>
+          {note !== null && (
+             <>
+              <h1>note_data : </h1>
+          <ObjectBox object={note} />
+              </>
+          )}
+          {note_sections !== null && (
+              <>
+              <h1>note sections :</h1>
+              {note_sections.map((note_section, index) => {
+                  return (
+                      <React.Fragment key={index}>
+                        <ObjectBox object={note_section}  />
+                  </React.Fragment>)
+              }
+                  )}
+                  </>
+          )}
+    </div>
   )
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-
+import { get_workflow_tasks } from '../../api/client'
+import ObjectBox from './ObjectBox'
 export const Task = () => {
-  var {task_id,workspace_id,workflow_id,username } = useParams()
+  var { task_id, workspace_id, workflow_id, username } = useParams()
+  var [task, set_task] = useState(null)
+  async function get_data() {
+    var workflow_tasks = await get_workflow_tasks({
+      workflow_id,
+      creator: username
+    })
+    set_task(workflow_tasks.find(task => task._id == task_id))
+  }
+  useEffect(() => {
+    get_data()
+  },[])
   return (
-    <div>Task</div>
+    <div>
+      <h1>Task</h1>
+      {task !== null && (
+        <ObjectBox object={task} /> 
+      )}
+    </div>
   )
 }
