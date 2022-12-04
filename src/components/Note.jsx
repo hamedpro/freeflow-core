5ba5b477320fb6d29479ff12ddd791997c12f3b9
York-Citynew <y.c.new.a@gmail.com>
2022-12-04 15:52:06 +0330

    added editorJs, editorJs functionality

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,66 +1,85 @@
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import Attach from "@editorjs/attaches";
+import Table from "@editorjs/table";
+import ImageTool from "@editorjs/image";
+import Checklist from "@editorjs/checklist";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { get_user_notes, get_note_sections, update_note } from "../../api/client";
+import { get_user_notes, update_note } from "../../api/client";
 import ObjectBox from "./ObjectBox";
 export const Note = () => {
-	var { note_id, workspace_id, workflow_id, user_id } = useParams();
-	var [note, set_note] = useState(null);
-	async function get_data() {
-		var tmp = (await get_user_notes({ creator_user_id: user_id })).find((note) => note._id == note_id);
-		set_note(tmp);
-		/* 
-			now when set_note is done you can see what this "note" state
-			looks like (visually) in return func of this component
-			(at the time of writing i mean line 25 )
-		*/
-		if (tmp.blocks) {
-			// it will just run if blocks is not undefined and also is not null( and also if its an array it should not be empty)
-			/* editor js's instance was initialized in mount useEffect
+  var { note_id, workspace_id, workflow_id, user_id } = useParams();
+  var [note, setNote] = useState(null);
+  const editor = new EditorJS({
+    holder: "editor-js-div",
+    tools: {
+      header: {
+        class: Header,
+        inlineToolbar: true,
+      },
+      list: {
+        class: List,
+        inlineToolbar: true,
+      },
+      attach: {
+        class: Attach,
+        inlineToolbar: true,
+      },
+      table: {
+        class: Table,
+        inlineToolbar: true,
+      },
+      image: {
+        class: ImageTool,
+        inlineToolbar: true,
+      },
+      checklist: {
+        class: Checklist,
+        inlineToolbar: true,
+      },
+    },
+    data: note,
+    defaultBlock: "header",
+    autofocus: true,
+    placeholder: "Type your text here",
+  });
+  async function get_data() {
+    var tmp = (await get_user_notes({ creator_user_id: user_id })).find(
+      (note) => note._id == note_id
+    );
+    setNote(tmp);
+    if (tmp.blocks) {
+      // it will just run if blocks is not undefined and also is not null( and also if its an array it should not be empty)
+      /* editor js's instance was initialized in mount useEffect
 			so render these blocks into that */
-			//use tmp.blocks here instead of note.blocks because set_state is async and may note has not changed yet 
-		}
-	}
-	useEffect(() => {
-		var editor_js_div = document.getElementById('editor_js_div')
-		//initialize an empty editor js instance here
-		get_data();
-	}, []);
-	async function push_blocks() {
-		/* 
-			whenever user clicks to save changes or when auto save is on this function
-			show be called 
-		*/
-
-		/*
-			what to do ? get data from editorjs's .save method and use 
-			update_note (that's already imported as api_update_note) like this : 
-
-		await update_note({
-			note_id,
-			update_set: {
-				blocks : "blocks object which is inside editor js's .save() method's reuslt "
-			}
-		})
-		*/
-	}
-	return (
-		<div>
-            <h1>Note</h1>
-            <div>
-                <p>workspace_id : {workspace_id }</p>
-                <p>workflow_id : {workflow_id }</p>
-            </div>
-			{note !== null && (
-				<>
-					<h1>note_data : </h1>
-					<ObjectBox object={note} />
-				</>
-			)}
-			<h2>note blocks part</h2>
-			<div id="editor_js_div">
-				{/* give id of this div to editor js and
-				it will take control of it and knows what to do*/}
-			</div>
-		</div>
-	);
+      //use tmp.blocks here instead of note.blocks because set_state is async and may note has not changed yet
+    }
+  }
+  useEffect(() => {
+    get_data();
+  }, []);
+  const saveHandler = async () => {
+    await update_note({ note_id, update_set: editor.save() });
+    //TODO: auto save
+  };
+  return (
+    <div>
+      <h1>Note</h1>
+      <div>
+        <p>workspace_id : {workspace_id}</p>
+        <p>workflow_id : {workflow_id}</p>
+      </div>
+      {note !== null && (
+        <>
+          <h1>note_data : </h1>
+          <ObjectBox object={note} />
+        </>
+      )}
+      <h2>note blocks part</h2>
+      <div id='editor-js-div'></div>
+      <button onClick={saveHandler}>save</button>
+    </div>
+  );
 };
