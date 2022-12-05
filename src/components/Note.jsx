8fbadb889872a076a8abebff7ea141e858b353e9
hamed yaghoutpour <hamedpro30@gmail.com>
2022-12-05 14:24:56 +0330

    fixing problems of adding editor js functionality (the last commit)

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -10,76 +10,87 @@ import { useParams } from "react-router-dom";
 import { get_user_notes, update_note } from "../../api/client";
 import ObjectBox from "./ObjectBox";
 export const Note = () => {
-  var { note_id, workspace_id, workflow_id, user_id } = useParams();
-  var [note, setNote] = useState(null);
-  const editor = new EditorJS({
-    holder: "editor-js-div",
-    tools: {
-      header: {
-        class: Header,
-        inlineToolbar: true,
-      },
-      list: {
-        class: List,
-        inlineToolbar: true,
-      },
-      attach: {
-        class: Attach,
-        inlineToolbar: true,
-      },
-      table: {
-        class: Table,
-        inlineToolbar: true,
-      },
-      image: {
-        class: ImageTool,
-        inlineToolbar: true,
-      },
-      checklist: {
-        class: Checklist,
-        inlineToolbar: true,
-      },
-    },
-    data: note,
-    defaultBlock: "header",
-    autofocus: true,
-    placeholder: "Type your text here",
-  });
-  async function get_data() {
-    var tmp = (await get_user_notes({ creator_user_id: user_id })).find(
-      (note) => note._id == note_id
-    );
-    setNote(tmp);
-    if (tmp.blocks) {
-      // it will just run if blocks is not undefined and also is not null( and also if its an array it should not be empty)
-      /* editor js's instance was initialized in mount useEffect
-			so render these blocks into that */
-      //use tmp.blocks here instead of note.blocks because set_state is async and may note has not changed yet
-    }
-  }
-  useEffect(() => {
-    get_data();
-  }, []);
-  const saveHandler = async () => {
-    await update_note({ note_id, update_set: editor.save() });
-    //TODO: auto save
-  };
-  return (
-    <div>
-      <h1>Note</h1>
-      <div>
-        <p>workspace_id : {workspace_id}</p>
-        <p>workflow_id : {workflow_id}</p>
-      </div>
-      {note !== null && (
-        <>
-          <h1>note_data : </h1>
-          <ObjectBox object={note} />
-        </>
-      )}
-      <h2>note blocks part</h2>
-      <div id='editor-js-div'></div>
-      <button onClick={saveHandler}>save</button>
-    </div>
-  );
+	var { note_id, workspace_id, workflow_id, user_id } = useParams();
+	var [note, setNote] = useState(null);
+	var [editor_js_instanse, set_editor_js_instance] = useState(null);
+
+	async function init_component() {
+		var tmp = (await get_user_notes({ creator_user_id: user_id })).find(
+			(note) => note._id == note_id
+		);
+		setNote(tmp);
+		var editor_js_configs = {
+			holder: "editor-js-div",
+			tools: {
+				header: {
+					class: Header,
+					inlineToolbar: true,
+				},
+				list: {
+					class: List,
+					inlineToolbar: true,
+				},
+				attach: {
+					class: Attach,
+					inlineToolbar: true,
+				},
+				table: {
+					class: Table,
+					inlineToolbar: true,
+				},
+				image: {
+					class: ImageTool,
+					inlineToolbar: true,
+				},
+				checklist: {
+					class: Checklist,
+					inlineToolbar: true,
+				},
+			},
+			onReady: () => {
+				console.log("editor js initializing is done.");
+				//todo show this console.log like a notification or ... to user
+			},
+			defaultBlock: "header",
+			autofocus: true,
+			placeholder: "Type your text here",
+		};
+		if (tmp.data) {
+			editor_js_configs["data"] = tmp.data;
+		}
+		set_editor_js_instance(new EditorJS(editor_js_configs));
+	}
+	useEffect(() => {
+		init_component();
+	}, []);
+	const saveHandler = async () => {
+		editor_js_instanse.save().then((output_data) => {
+			try {
+				update_note({ note_id, update_set: { data: output_data } });
+				alert("all done");
+			} catch (error) {
+				console.log(error);
+				alert(
+					"something went wrong when saving the new edited note data. details in console"
+				);
+			}
+		});
+		/* 
+      TODO: auto save : pass onChange prop to editor_js_configs before initializing and save changes in that onChange
+      and also show an indicator which whenever the data changes it shows loading until the data changes is uploaded succeessfuly
+    */
+	};
+	return (
+		<>
+			<h1>Note</h1>
+			{note !== null && (
+				<>
+					<h1>note_data : </h1>
+					<ObjectBox object={note} />
+				</>
+			)}
+			<div id="editor-js-div"></div>
+			<button onClick={saveHandler}>save</button>
+		</>
+	);
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppContainer } from "./AppContainer";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<AppContainer />
-	</React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<AppContainer />);
