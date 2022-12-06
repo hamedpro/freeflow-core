8215b27b7eec29ec8b8e5e19240361d8233f0fa4
hamed yaghoutpour <hamedpro30@gmail.com>
2022-12-06 15:15:47 +0330

    fixing a problem : on route change for ex from /users/222 to /users/444 it didnt re render the element

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import "./output.css";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation, useParams } from "react-router-dom";
 //import {} from "./components";
 import { multi_lang_helper as ml } from "./common";
 import { Login } from "./components/Login";
@@ -27,55 +27,56 @@ import { DayCalendar } from "./components/DayCalendar";
 import { AdminDashboardUsersSection } from "./components/AdminDashboardUsersSection";
 import { Root } from "./components/Root.jsx";
 import { PrimarySideBar } from "./components/PrimarySideBar";
+import { useEffect } from "react";
 function Wrapper() {
 	return (
 		<div className="h-full w-full border-black-900 flex">
-			<div className="w-1/5 bg-blue-400 h-full">
+			<div className="w-1/5 bg-blue-500 h-full">
 				<PrimarySideBar />
 			</div>
 			<div className="w-4/5 bg-blue-400 h-full">
 				<Routes>
-					<Route path="" element={<UserDashboard />} />
+					<Route path="" element={<UserDashboard timestamp={new Date().getTime()} />} />
 					<Route
 						path="complete_user_registering"
-						element={<RegisterCompleteUserInfo />}
+						element={<RegisterCompleteUserInfo timestamp={new Date().getTime()} />}
 					/>
-					<Route path="verification" element={<VerifyIdentity />} />
-					<Route path="workspaces" element={<WorkspacesPage />} />
-					<Route path="workspaces/new" element={<NewWorkspace />} />
-					<Route path="workspaces/:workspace_id" element={<WorkspacePage />} />
+					<Route path="verification" element={<VerifyIdentity timestamp={new Date().getTime()} />} />
+					<Route path="workspaces" element={<WorkspacesPage timestamp={new Date().getTime()} />} />
+					<Route path="workspaces/new" element={<NewWorkspace timestamp={new Date().getTime()} />} />
+					<Route path="workspaces/:workspace_id" element={<WorkspacePage timestamp={new Date().getTime()} />} />
 					<Route
 						path="workspaces/:workspace_id/workflows/new"
-						element={<NewWorkflow />}
+						element={<NewWorkflow timestamp={new Date().getTime()} />}
 					/>
 					<Route
 						path="workspaces/:workspace_id/workflows/:workflow_id"
-						element={<Workflow />}
+						element={<Workflow timestamp={new Date().getTime()} />}
 					/>
 					<Route
 						path="workspaces/:workspace_id/workflows/:workflow_id/notes/new"
-						element={<NewNote />}
+						element={<NewNote timestamp={new Date().getTime()} />}
 					/>
 					<Route
 						path="workspaces/:workspace_id/workflows/:workflow_id/notes/:note_id"
-						element={<Note />}
+						element={<Note timestamp={new Date().getTime()} />}
 					/>
 					<Route
 						path="workspaces/:workspace_id/workflows/:workflow_id/tasks/new"
-						element={<NewTask />}
+						element={<NewTask timestamp={new Date().getTime()} />}
 					/>
 					<Route
 						path="workspaces/:workspace_id/workflows/:workflow_id/tasks/:task_id"
-						element={<Task />}
+						element={<Task timestamp={new Date().getTime()} />}
 					/>
 					<Route path="calendar">
-						<Route path="year" element={<YearCalendar />} />
+						<Route path="year" element={<YearCalendar timestamp={new Date().getTime()} />} />
 						{/* todo : test it */}
-						<Route path="month" element={<MonthCalendar />} />
+						<Route path="month" element={<MonthCalendar timestamp={new Date().getTime()} />} />
 						{/* todo : test it */}
-						<Route path="week" element={<WeekCalendar />} />
+						<Route path="week" element={<WeekCalendar timestamp={new Date().getTime()} />} />
 						{/* todo : test it */}
-						<Route path="day" element={<DayCalendar />} />
+						<Route path="day" element={<DayCalendar timestamp={new Date().getTime()} />} />
 						{/* todo : test it */}
 					</Route>
 				</Routes>
@@ -86,17 +87,17 @@ function Wrapper() {
 function App() {
 	window.ml = ml;
 	window.api_endpoint = API_ENDPOINT; // it gets replaced by vite
-
+	var loc = useLocation();
 	return (
 		<Routes>
-			<Route path="/" element={<Root />} />
-			<Route path="/login" element={<Login />} />
-			<Route path="/register" element={<RegisterPage />} />
-			<Route path="/terms" element={<Terms />} />
-			<Route path="subscribtion" element={<SubscribtionPage />} />
-			<Route path="/users/:user_id/*" element={<Wrapper />}></Route>
+			<Route path="/" element={<Root timestamp={new Date().getTime()} />} />
+			<Route path="/login" element={<Login timestamp={new Date().getTime()} />} />
+			<Route path="/register" element={<RegisterPage timestamp={new Date().getTime()} />} />
+			<Route path="/terms" element={<Terms timestamp={new Date().getTime()} />} />
+			<Route path="subscribtion" element={<SubscribtionPage timestamp={new Date().getTime()} />} />
+			<Route path="/users/:user_id/*" element={<Wrapper timestamp={new Date().getTime()} />}></Route>
 			{/* <Route path="/admin">
-				<Route path="users" element={<AdminDashboardUsersSection />} />
+				<Route path="users" element={<AdminDashboardUsersSection timestamp={new Date().getTime()} />} />
 			</Route> */}{" "}
 			{/* todo instead of having a seperated route for admin, 
 			when loading profile of each user check if his/her account has admin previleges or not
