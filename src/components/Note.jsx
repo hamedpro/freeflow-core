603f228d02f89aa2b636836af93d5a576484217a
York-Citynew <y.c.new.a@gmail.com>
2022-12-21 22:43:20 +0330

    added comments ui system to note, task, workflow and workspace components

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -9,88 +9,93 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { get_user_notes, update_note } from "../../api/client";
 import ObjectBox from "./ObjectBox";
+import CommentsBox from "./CommentsBox";
 export const Note = () => {
-	var { note_id, workspace_id, workflow_id, user_id } = useParams();
-	var [note, setNote] = useState(null);
-	var [editor_js_instanse, set_editor_js_instance] = useState(null);
+  var { note_id, workspace_id, workflow_id, user_id } = useParams();
+  var [note, setNote] = useState(null);
+  var [editor_js_instanse, set_editor_js_instance] = useState(null);
 
-	async function init_component() {
-		var tmp = (await get_user_notes({ creator_user_id: user_id })).find(
-			(note) => note._id == note_id
-		);
-		setNote(tmp);
-		var editor_js_configs = {
-			holder: "editor-js-div",
-			tools: {
-				header: {
-					class: Header,
-					inlineToolbar: true,
-				},
-				list: {
-					class: List,
-					inlineToolbar: true,
-				},
-				attach: {
-					class: Attach,
-					inlineToolbar: true,
-				},
-				table: {
-					class: Table,
-					inlineToolbar: true,
-				},
-				image: {
-					class: ImageTool,
-					inlineToolbar: true,
-				},
-				checklist: {
-					class: Checklist,
-					inlineToolbar: true,
-				},
-			},
-			onReady: () => {
-				console.log("editor js initializing is done.");
-				//todo show this console.log like a notification or ... to user
-			},
-			defaultBlock: "header",
-			autofocus: true,
-			placeholder: "Type your text here",
-		};
-		if (tmp.data) {
-			editor_js_configs["data"] = tmp.data;
-		}
-		set_editor_js_instance(new EditorJS(editor_js_configs));
-	}
-	useEffect(() => {
-		init_component();
-	}, []);
-	const saveHandler = async () => {
-		editor_js_instanse.save().then((output_data) => {
-			try {
-				update_note({ note_id, update_set: { data: output_data } });
-				alert("all done");
-			} catch (error) {
-				console.log(error);
-				alert(
-					"something went wrong when saving the new edited note data. details in console"
-				);
-			}
-		});
-		/* 
+  async function init_component() {
+    var tmp = (await get_user_notes({ creator_user_id: user_id })).find(
+      (note) => note._id == note_id
+    );
+    setNote(tmp);
+    var editor_js_configs = {
+      holder: "editor-js-div",
+      tools: {
+        header: {
+          class: Header,
+          inlineToolbar: true,
+        },
+        list: {
+          class: List,
+          inlineToolbar: true,
+        },
+        attach: {
+          class: Attach,
+          inlineToolbar: true,
+        },
+        table: {
+          class: Table,
+          inlineToolbar: true,
+        },
+        image: {
+          class: ImageTool,
+          inlineToolbar: true,
+        },
+        checklist: {
+          class: Checklist,
+          inlineToolbar: true,
+        },
+      },
+      onReady: () => {
+        console.log("editor js initializing is done.");
+        //todo show this console.log like a notification or ... to user
+      },
+      defaultBlock: "header",
+      autofocus: true,
+      placeholder: "Type your text here",
+    };
+    if (tmp.data) {
+      editor_js_configs["data"] = tmp.data;
+    }
+    set_editor_js_instance(new EditorJS(editor_js_configs));
+  }
+  useEffect(() => {
+    init_component();
+  }, []);
+  const saveHandler = async () => {
+    editor_js_instanse.save().then((output_data) => {
+      try {
+        update_note({ note_id, update_set: { data: output_data } });
+        alert("all done");
+      } catch (error) {
+        console.log(error);
+        alert(
+          "something went wrong when saving the new edited note data. details in console"
+        );
+      }
+    });
+    /* 
       TODO: auto save : pass onChange prop to editor_js_configs before initializing and save changes in that onChange
       and also show an indicator which whenever the data changes it shows loading until the data changes is uploaded succeessfuly
     */
-	};
-	return (
-		<>
-			<h1>Note</h1>
-			{note !== null && (
-				<>
-					<h1>note_data : </h1>
-					<ObjectBox object={note} />
-				</>
-			)}
-			<div id="editor-js-div"></div>
-			<button onClick={saveHandler}>save</button>
-		</>
-	);
+  };
+  return (
+    <>
+      <h1>Note</h1>
+      {note !== null && (
+        <>
+          <h1>note_data : </h1>
+          <ObjectBox object={note} />
+        </>
+      )}
+      <div id='editor-js-div'></div>
+      <button onClick={saveHandler}>save</button>
+      <CommentsBox
+        user_id={user_id}
+        urlParams={useParams()}
+      />
+    </>
+  );
 };
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,28 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { get_tasks } from '../../api/client'
-import ObjectBox from './ObjectBox'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { get_tasks } from "../../api/client";
+import CommentsBox from "./CommentsBox";
+import ObjectBox from "./ObjectBox";
 export const Task = () => {
-  var { task_id, workspace_id, workflow_id, user_id } = useParams()
-  var [task, set_task] = useState(null)
+  var { task_id, workspace_id, workflow_id, user_id } = useParams();
+  var [task, set_task] = useState(null);
   async function get_data() {
     var workflow_tasks = await get_tasks({
       filters: {
-        workflow_id
-      }
-      
-    })
-    set_task(workflow_tasks.find(task => task._id == task_id))
+        workflow_id,
+      },
+    });
+    set_task(workflow_tasks.find((task) => task._id == task_id));
   }
   useEffect(() => {
-    get_data()
-  },[])
+    get_data();
+  }, []);
   return (
     <div>
       <h1>Task</h1>
-      {task !== null && (
-        <ObjectBox object={task} /> 
-      )}
+      {task !== null && <ObjectBox object={task} />}
+      <CommentsBox
+        user_id={user_id}
+        urlParams={useParams()}
+      />
     </div>
-  )
-}
+  );
+};
