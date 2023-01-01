1503615f00af6b28ee61e75572080f80b4b1afed
hamed yaghoutpour <hamedpro30@gmail.com>
2023-01-01 23:23:34 +0330

    formatting collaborators management box component and working a lot on system of uploading new resource

--- src/components/CollaboratorsManagementBox.jsx
+++ src/components/CollaboratorsManagementBox.jsx
@@ -3,51 +3,80 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { get_collection, modify_collaborator_access_level, update_document } from "../../api/client";
+import {
+	get_collection,
+	modify_collaborator_access_level,
+	update_document,
+} from "../../api/client";
 import { Section } from "./section.jsx";
-import Select from "react-select"
-function OptionsSection({collaborators,id,context,get_data_function}) {
-    var [all_users, set_all_users] = useState(null)
-    var [selected_collaborators, set_selected_collaborators] = useState(null)
-    async function get_data() {
-        set_all_users(await get_collection({ collection_name: "users", filters: {} }))
-        get_data_function()
-    }
-    useEffect(() => {
-        get_data()
-       },[])
-    async function add_new_collaborators() {
-        await update_document({
-            collection: context,
-            update_filter: {
-                _id : id 
-            },
-            update_set: {
-                collaborators : [...collaborators.map(i=>{return {user_id : i.user_id,access_level : i.access_level}}),...selected_collaborators.map(i=>{return {user_id : i.value,access_level : 1}})]
-            }
-        })
-        alert('all done!')
-        get_data()
-    }
+import Select from "react-select";
+function OptionsSection({ collaborators, id, context, get_data_function }) {
+	var [all_users, set_all_users] = useState(null);
+	var [selected_collaborators, set_selected_collaborators] = useState(null);
+	async function get_data() {
+		set_all_users(await get_collection({ collection_name: "users", filters: {} }));
+		get_data_function();
+	}
+	useEffect(() => {
+		get_data();
+	}, []);
+	async function add_new_collaborators() {
+		await update_document({
+			collection: context,
+			update_filter: {
+				_id: id,
+			},
+			update_set: {
+				collaborators: [
+					...collaborators.map((i) => {
+						return { user_id: i.user_id, access_level: i.access_level };
+					}),
+					...selected_collaborators.map((i) => {
+						return { user_id: i.value, access_level: 1 };
+					}),
+				],
+			},
+		});
+		alert("all done!");
+		get_data();
+	}
 
-    if (all_users === null) return <h1>loading all_users...</h1>
-    return (
-        <><Section title="options">
-            <Select
-                onChange={set_selected_collaborators}
-                value={selected_collaborators}
-                options={[
-                    ...all_users.filter(user => !collaborators.map(i => i.user_id).includes(user._id)).map((user) => {
-                        return {
-                            value: user._id,
-                            label: `@${user.username}`,
-                        };
-                    }),
-                ]}
-                isMulti
-                isSearchable />
-        </Section><button onClick={add_new_collaborators}>add these new collaborators</button></>
-    )
+	if (all_users === null) return <h1>loading all_users...</h1>;
+	return (
+		<>
+			<Section title="options">
+				<h1>adding new collaborators</h1>
+				<p>
+					select users that you want to add as new collaborators of this{" "}
+					{context.slice(0, context.length - 1)} and hit the submit button :
+				</p>
+				<Select
+					onChange={set_selected_collaborators}
+					value={selected_collaborators}
+					options={[
+						...all_users
+							.filter(
+								(user) => !collaborators.map((i) => i.user_id).includes(user._id)
+							)
+							.map((user) => {
+								return {
+									value: user._id,
+									label: `@${user.username}`,
+								};
+							}),
+					]}
+					isMulti
+					isSearchable
+				/>
+				<button
+					className="border border-stone-600 rounded mt-2 px-2"
+					onClick={add_new_collaborators}
+				>
+					add these new collaborators
+				</button>
+			</Section>
+		</>
+	);
 }
 export const CollaboratorsManagementBox = ({ context, id }) => {
 	//for example if you want to show collaborators of a workspace with id "blah" : context : "workspaces" , id : "blah"
@@ -85,16 +114,30 @@ export const CollaboratorsManagementBox = ({ context, id }) => {
 		//mode === 1 : upgrade access level of that collaborator by 1
 		//mode === -1 : downgrade access level of that collaborator by 1
 		if (!window.confirm("are you sure ?!")) return;
-        if (collaborator.access_level + mode === 3) {
-            if (!window.confirm('now you are owner but if you do this action you become an admin and this user with be owner. do you want to continue ? ')) {
-                return 
-            } else {
-                await modify_collaborator_access_level({ context, id, user_id: localStorage.getItem('user_id'), new_access_level: 2 })
-                alert('your access level was changed to 2 ("admin").')
-            }
-        }
+		if (collaborator.access_level + mode === 3) {
+			if (
+				!window.confirm(
+					"now you are owner but if you do this action you become an admin and this user with be owner. do you want to continue ? "
+				)
+			) {
+				return;
+			} else {
+				await modify_collaborator_access_level({
+					context,
+					id,
+					user_id: localStorage.getItem("user_id"),
+					new_access_level: 2,
+				});
+				alert('your access level was changed to 2 ("admin").');
+			}
+		}
 		try {
-           await  modify_collaborator_access_level({context,id,user_id : collaborator.user_id,new_access_level :collaborator.access_level + mode  })
+			await modify_collaborator_access_level({
+				context,
+				id,
+				user_id: collaborator.user_id,
+				new_access_level: collaborator.access_level + mode,
+			});
 			alert("all done!");
 		} catch (error) {
 			alert("something went wrong! details in console");
@@ -108,72 +151,81 @@ export const CollaboratorsManagementBox = ({ context, id }) => {
 	if (collaborators === null) return <h1>loading collaborators...</h1>;
 	return (
 		<Section title="Collaborators Management Box">
-            <Section title="current collaborators">
-            {collaborators.map((collaborator, index) => {
-				var logged_in_user = collaborators.find(
-					(coll) => coll.user_id === window.localStorage.getItem("user_id")
-				);
-				return (
-					<div
-						key={index}
-						className="h-16 flex items-center space-x-4 flex-1 overflow-scroll"
-					>
-						<h1>#{index + 1}</h1>
-						{collaborator.user_document.profile_image ? (
-							<img
-								className="h-14 w-14 rounded-full"
-								src={`${window.API_ENDPOINT}/profile_images/${collaborator.user_document.profile_image}`}
-							/>
-						) : (
-							<PersonRounded className="h-14 w-14 bg-white" />
-						)}
-						<p>
-							username :{" "}
-							{collaborator.user_document.username ? (
-								<Link to={`/users/${collaborator.user_id}`}>
-									@{collaborator.user_document.username}
-								</Link>
+			<Section title="current collaborators">
+				{collaborators.map((collaborator, index) => {
+					var logged_in_user = collaborators.find(
+						(coll) => coll.user_id === window.localStorage.getItem("user_id")
+					);
+					return (
+						<div
+							key={index}
+							className="h-16 flex items-center space-x-4 flex-1 overflow-scroll"
+						>
+							<h1>#{index + 1}</h1>
+							{collaborator.user_document.profile_image ? (
+								<img
+									className="h-14 w-14 rounded-full"
+									src={`${window.API_ENDPOINT}/profile_images/${collaborator.user_document.profile_image}`}
+								/>
 							) : (
-								<p>undefined</p>
+								<PersonRounded className="h-14 w-14 bg-white" />
 							)}
-						</p>
-						<p>
-							privilege level :{" "}
-							{collaborator.access_level === 3
-								? "owner"
-								: collaborator.access_level === 2
-								? "admin"
-								: "normal user"}
-						</p>
-						{logged_in_user.access_level > collaborator.access_level ? (
-							<>
-								<button
-									onClick={() =>
-										remove_collaborator_handler(collaborator.user_id)
-									}
-								>
-									remove collaborator
-								</button>
-								<button onClick={() => change_user_access_level(collaborator, 1)}>
-									{collaborator.access_level === 1 ? "make user admin" : null}
-									{collaborator.access_level === 2 ? "make user owner" : null}
-								</button>
-								{collaborator.access_level !== 1 ? (
+							<p>
+								username :{" "}
+								{collaborator.user_document.username ? (
+									<Link to={`/users/${collaborator.user_id}`}>
+										@{collaborator.user_document.username}
+									</Link>
+								) : (
+									<p>undefined</p>
+								)}
+							</p>
+							<p>
+								privilege level :{" "}
+								{collaborator.access_level === 3
+									? "owner"
+									: collaborator.access_level === 2
+									? "admin"
+									: "normal user"}
+							</p>
+							{logged_in_user.access_level > collaborator.access_level ? (
+								<>
 									<button
-										onClick={() => change_user_access_level(collaborator, -1)}
+										onClick={() =>
+											remove_collaborator_handler(collaborator.user_id)
+										}
 									>
-										{collaborator.access_level === 2
-											? "make user normal user"
-											: null}
+										remove collaborator
 									</button>
-								) : null}
-							</>
-						) : null}
-					</div>
-				);
-            })}
-            </Section>
-            <OptionsSection collaborators={collaborators} context={context} id={id} get_data_function={ get_data} /> 
+									<button
+										onClick={() => change_user_access_level(collaborator, 1)}
+									>
+										{collaborator.access_level === 1 ? "make user admin" : null}
+										{collaborator.access_level === 2 ? "make user owner" : null}
+									</button>
+									{collaborator.access_level !== 1 ? (
+										<button
+											onClick={() =>
+												change_user_access_level(collaborator, -1)
+											}
+										>
+											{collaborator.access_level === 2
+												? "make user normal user"
+												: null}
+										</button>
+									) : null}
+								</>
+							) : null}
+						</div>
+					);
+				})}
+			</Section>
+			<OptionsSection
+				collaborators={collaborators}
+				context={context}
+				id={id}
+				get_data_function={get_data}
+			/>
 		</Section>
 	);
 };
--- src/components/NewResource.jsx
+++ src/components/NewResource.jsx
@@ -1,27 +1,49 @@
 import React, { useEffect, useState } from "react";
-import { useMatch, useParams, useSearchParams } from "react-router-dom";
+import { useMatch, useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { get_users, upload_new_resources } from "../../api/client";
 import Select from "react-select";
 export const NewResource = () => {
+	var nav = useNavigate();
 	var [search_params, set_search_params] = useSearchParams();
 
 	var workspace_id = search_params.get("workspace_id");
 	var workflow_id = search_params.get("workflow_id");
 
 	var user_id = localStorage.getItem("user_id");
-	function upload_files_handler() {
+	async function upload_files_handler() {
+		if (document.getElementById("files_input").files.length === 0) {
+			alert("you have not selected any file to upload.");
+			return;
+		}
 		var collaborators = selected_collaborators.map((i) => {
 			return { access_level: 1, user_id: i.value };
 		});
 		collaborators.push({ access_level: 3, user_id });
-		upload_new_resources({
-			input_element_id: "files_input",
-			data: {
-				workspace_id,
-				workflow_id,
-				collaborators,
-			},
-		});
+		try {
+			var result = await upload_new_resources({
+				input_element_id: "files_input",
+				data: {
+					workspace_id,
+					workflow_id,
+					collaborators,
+				},
+			});
+			alert(
+				`all done! ${
+					result.length === 1
+						? "navigating to this new uploaded resource ..."
+						: "you have uploaded multiple files so you will be navigated to workflow of these new uploaded files..."
+				}`
+			);
+			if (result.length === 1) {
+				nav(`/dashboard/resources/${result[0]}`);
+			} else {
+				nav(`/dashboard/workflows/${workflow_id}`);
+			}
+		} catch (error) {
+			console.log(error);
+			alert("something went wrong. details in console");
+		}
 	}
 	var [all_users, set_all_users] = useState(null);
 	async function get_data() {
@@ -42,12 +64,14 @@ export const NewResource = () => {
 				onChange={set_selected_collaborators}
 				value={selected_collaborators}
 				options={[
-					...all_users.filter(user => user._id !== user_id).map((user) => {
-						return {
-							value: user._id,
-							label: `@${user.username}`,
-						};
-					}),
+					...all_users
+						.filter((user) => user._id !== user_id)
+						.map((user) => {
+							return {
+								value: user._id,
+								label: `@${user.username}`,
+							};
+						}),
 				]}
 				isMulti
 				isSearchable
