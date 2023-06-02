e27095db98521b286e1cf8a1797e0df71633a7ee
hamed yaghoutpour <hamedpro30@gmail.com>
2023-06-02 10:34:45 +0330

    added missing tsc to start script

--- start_script.bash
+++ start_script.bash
@@ -3,6 +3,7 @@ if [[ -f "~/.pink_rose_data/env.json" ]]; then
     echo "'~/.pink_rose_data/env.json' does not exist. create it and try again"; 
     exit 1 ; 
 fi; 
+tsc ;
 node ./api_dist/api/server.js &
 tailwindcss -c ./tailwind.config.cjs -i ./src/input.css -o ./src/output.css ;
 vite build ;
